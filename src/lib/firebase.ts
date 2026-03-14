5ff238b14b9a505d2f237ea3d49a16444eb2fc14
seidullakb <seidullakb@gmail.com>
2026-03-14 22:13:47 +0500

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,14 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCm3Q_sq2ZUnkphqhGj3bU24bXkFtJrxSo",
-  authDomain: "eco-project-5f7ad.firebaseapp.com",
-  projectId: "eco-project-5f7ad",
-  storageBucket: "eco-project-5f7ad.firebasestorage.app",
-  messagingSenderId: "265580291341",
-  appId: "1:265580291341:web:2de5c8a675ea7f2d973c44",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
