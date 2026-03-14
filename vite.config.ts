1ab2a7420bfaedb5975cdd9669587160f6e1dd22
seidullakb <seidullakb@gmail.com>
2026-03-15 00:25:26 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
-});
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000, // This increases the limit from 500kb to 1000kb
+    chunkSizeWarningLimit: 1000,
   },
 })
