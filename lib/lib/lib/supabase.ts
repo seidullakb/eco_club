888875aa62b966fe6cbe7ad908f513b1f8bd6a25
seidullakb <seidullakb@gmail.com>
2026-03-14 19:40:14 +0500

    Create supabase.ts

---
+++ lib/lib/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-all';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
